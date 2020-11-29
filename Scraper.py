72376b36f2218300b732e4008477408aec325fb9
Westriel <westriel@gmail.com>
2020-11-29 15:27:56 -0500

    Added comments to db

--- Scraper.py
+++ Scraper.py
@@ -14,6 +14,15 @@ CREATE TABLE IF NOT EXISTS "post" (
     "attachment" TEXT,
     PRIMARY KEY("permalink")
 )''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS "comment" (
+    "id" TEXT,
+    "content" TEXT,
+    "postlink" TEXT,
+    "permalink" TEXT,
+    FOREIGN KEY("postlink") REFERENCES "post",
+    PRIMARY KEY("id")
+)''')
 
 
 while(True):
@@ -32,5 +41,17 @@ while(True):
             info = x["data"]
             cursor.execute('''INSERT INTO post(subreddit, title, content, permalink, attachment) VALUES(?,?,?,?,?)''',
                            (info["subreddit"],info["title"],info["selftext"],info["permalink"],info["url"]))
+            commentsReq = requests.get("https://www.reddit.com/" + info['permalink'] + ".json",headers = {'User-agent':'Hacksu Scraper'})
+            if(not commentsReq.ok):
+                print("Got Error code: "+str(commentsReq.status_code))
+            else:
+                comments = commentsReq.json()[1]['data']['children']
+                for comment in comments:
+                    if comment['kind'] != 'more':
+                        cinfo = comment['data']
+                        cursor.execute('''INSERT INTO comment(id, content, postlink, permalink) VALUES(?,?,?,?)''',(cinfo['id'], cinfo['body'], info['permalink'], cinfo['permalink']))
+                        # print(comment['data']['body'])
+
+
         db.commit()
         print("Done!")
